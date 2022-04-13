2bc028a3f2ef07e1cc6638934f8e9af79d3e8a83
tanyaleepr <tanyaleepr@gmail.com>
2022-04-12 20:43:38 -0500

    added comment model with ids

---
+++ models/Comment.js
@@ -0,0 +1,19 @@
+const { Schema, model } = require('mongoose');
+
+const CommentSchema = new Schema({
+  writtenBy: {
+    type: String
+  },
+  commentBody: {
+    type: String
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const Pizza = require('./Pizza');
+const Comment = require('./Comment');
+
+module.exports = { Pizza, Comment };
